ac9c4753d1d9b23fd61d58ee9624f554dd052eb8
Emeri Chifum <chifum@gmail.com>
2024-01-20 08:06:11 +1000

    Contact Us Update

---
+++ src/components/contactUsComponent/ContactUsContainer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Connect from "./Connect";
+
+const ContactUsContainer = () => {
+  return (
+    <div className="rounded-b-lg mb-60 bg-primary">
+    yes
+      <div className="container">
+      yes
+      </div>
+    </div>
+  );
+};
+
+export default ContactUsContainer;
+// <Connect />
